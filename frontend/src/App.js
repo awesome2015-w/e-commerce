566a6e5510dd610568820ded8b53f828be795dc0
Louay Mezlini <dbouazza68@gmail.com>
2024-01-06 18:53:52 +0100

    front-end updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import {Container} from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {Outlet} from 'react-router-dom'
+
 const App = () => {
   return (
     <>
     <Header /> 
      <main className="py-3">
       <Container>
-        <h1> Welcome to ProShop</h1>
-      </Container>
+          <Outlet/>
+        </Container>
      </main>
      <Footer />
     </>
